b4bbd537f1e1818e5d7a9e6365786524cd183b75
petitsurfeur <root@srv-ubuntu-02.cyb.pw>
2020-07-05 13:34:10 +0200

    custom couleurs

--- reload.sh
+++ reload.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-echo -e "Arret des containers"
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+echo -e "${GREEN}### Arret des containers${NOCOLOR}"
 docker stop grafana node_exporter blackbox_exporter prometheus alertmanager cadvisor
-echo -e "Suppression des containers"
+
+echo ""
+echo -e "${GREEN}### Suppression des containers${NOCOLOR}"
 docker rm grafana node_exporter blackbox_exporter prometheus alertmanager cadvisor
-echo -e "Liste des containers actifs"
-docker ps -a
-echo -e "Creation et lancement des containers"
+
+echo ""
+echo -e "${GREEN}### Creation et lancement des containers${NOCOLOR}"
 docker-compose up -d
+
+echo ""
+echo -e "${GREEN}### Liste des containers actifs${NOCOLOR}"
+docker ps -a
+
 
